54861df331a2de746dd80208102d6835ed5473cb
a <EYCHFSJD@gmail.com>
2023-04-11 20:13:43 +0800

    修复一个漏洞

--- src/main/java/me/oganesson/gregica/Gregica.java
+++ src/main/java/me/oganesson/gregica/Gregica.java
@@ -21,7 +21,7 @@ import org.apache.logging.log4j.Logger;
         modid = Gregica.MOD_ID,
         name = Gregica.MOD_NAME,
         version = Gregica.VERSION,
-        dependencies="after:thaumcraft"
+        dependencies="after:thaumcraft;after:gregtech"
 )
 public class Gregica {
 
