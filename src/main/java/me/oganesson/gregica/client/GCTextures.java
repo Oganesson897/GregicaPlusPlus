e317bcc6d5f86e4a3aa93aeeb5f34d08aff6b0f9
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-04-28 07:32:24 +0900

    我是傻逼

--- src/main/java/me/oganesson/gregica/client/GCTextures.java
+++ src/main/java/me/oganesson/gregica/client/GCTextures.java
@@ -22,11 +22,14 @@ public class GCTextures {
     public static SimpleSidedCubeRenderer LIGHTNING_ROD_ACTIVE_IV;
     public static OrientedOverlayRenderer QUBIT_COMPUTER_OVERLAY;
     public static OrientedOverlayRenderer RESEARCH_STATION_OVERLAY;
+    public static SimpleOverlayRenderer ASEPTIC_FARM_CASING;
+    public static OrientedOverlayRenderer ALGAE_FARM;
     public static final ModelResourceLocation LASER_PIPE_MODEL = new ModelResourceLocation(new ResourceLocation(Gregica.MOD_ID, "laser_pipe_normal"), "normal");
 
     public static void preInit() {
         MAGIC_CASING = new SimpleOverlayRenderer("magic_machine_casing");
         FISHING_CASING = new SimpleOverlayRenderer("fishing_machine_casing");
+        ASEPTIC_FARM_CASING = new SimpleOverlayRenderer("aseptic_farm_maching_casing");
         QUANTUM_CASING = new SimpleOverlayRenderer("computer_machine_casing");
         QUBIT_GENERATOR_CASING = new SimpleOverlayRenderer("quantum_generator_machine_casing");
         LARGE_ESSENTIA_GENERATOR = new OrientedOverlayRenderer("multiblock/large_essentia_generator", OrientedOverlayRenderer.OverlayFace.FRONT);
@@ -38,6 +41,7 @@ public class GCTextures {
         LIGHTNING_ROD_ACTIVE_IV = new SimpleSidedCubeRenderer("casings/lightning_rod_iv/active");
         QUBIT_COMPUTER_OVERLAY = new OrientedOverlayRenderer("multiblock/qubit_computer", OrientedOverlayRenderer.OverlayFace.FRONT);
         RESEARCH_STATION_OVERLAY= new OrientedOverlayRenderer("multiblock/research_station", OrientedOverlayRenderer.OverlayFace.FRONT);
+        ALGAE_FARM= new OrientedOverlayRenderer("multiblock/algae_farm", OrientedOverlayRenderer.OverlayFace.FRONT);
     }
 
 }
