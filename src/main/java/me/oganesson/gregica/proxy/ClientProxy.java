e6112bb58c3a92a992f044b8081b62ea0cd8ff1b
Oganesson897 <101081378+Darknight123MC@users.noreply.github.com>
2023-05-22 21:13:03 +0900

    Add Journey Map API compile(TODO)

--- src/main/java/me/oganesson/gregica/proxy/ClientProxy.java
+++ src/main/java/me/oganesson/gregica/proxy/ClientProxy.java
@@ -10,9 +10,12 @@ import me.oganesson.gregica.common.block.GCYSMetaBlocks;
 import me.oganesson.gregica.common.block.laserpipe.BlockLaserPipe;
 import me.oganesson.gregica.common.item.itemUpgrades;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.DefaultStateMapper;
+import net.minecraft.client.resources.IResource;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -20,6 +23,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import static me.oganesson.gregica.common.tileentities.mte.GCMetaEntities.LASER_PIPES;
 
