0e13b877361827646ca3635fc0da4562793a443d
themorningglory233 <114865431+themorningglory233@users.noreply.github.com>
2023-05-26 21:58:02 +0800

    加入UEV组件

--- src/main/java/me/oganesson/gregica/common/recipes/component/GCYSComponentRecipes.java
+++ src/main/java/me/oganesson/gregica/common/recipes/component/GCYSComponentRecipes.java
@@ -66,7 +66,7 @@ public class GCYSComponentRecipes {
                 .input(round, Neutronium, 16)
                 .input(stick, Neutronium, 4)
                 .input(gear, Neutronium)
-                .input(gearSmall, Neutronium, 2)
+                .input(gearSmall, Darmstadtium, 2)
                 .input(cableGtSingle, Europium, 2)
                 .fluidInputs(SolderingAlloy.getFluid(L * 4))
                 .fluidInputs(Lubricant.getFluid(1000))
@@ -79,7 +79,7 @@ public class GCYSComponentRecipes {
         ASSEMBLY_LINE_RECIPES.recipeBuilder()
                 .input(stickLong, Neutronium, 4)
                 .input(gear, Neutronium)
-                .input(gearSmall, Neutronium, 3)
+                .input(gearSmall, Darmstadtium, 3)
                 .input(ELECTRIC_MOTOR_UHV, 2)
                 .input(ELECTRIC_PISTON_UHV)
                 .input(circuit, MarkerMaterials.Tier.UHV)
@@ -157,6 +157,137 @@ public class GCYSComponentRecipes {
                 .duration(600).EUt(400000).buildAndRegister();
     }
 
+    private static void motor() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(stickLong, ChromiumGermaniumTellurideMagnetic)
+                .input(stickLong, MetallicHydrogen, 4)
+                .input(ring, MetallicHydrogen, 4)
+                .input(round, MetallicHydrogen, 8)
+                .input(wireFine, CarbonNanotube, 64)
+                .input(wireFine, CarbonNanotube, 64)
+                .input(cableGtSingle, PedotTma, 2)
+                .fluidInputs(SolderingAlloy.getFluid(L * 4))
+                .fluidInputs(Lubricant.getFluid(1000))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(ELECTRIC_MOTOR_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void conveyor() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(ELECTRIC_MOTOR_UEV, 2)
+                .input(plate, MetallicHydrogen, 2)
+                .input(ring, MetallicHydrogen, 4)
+                .input(round, MetallicHydrogen, 16)
+                .input(screw, MetallicHydrogen, 4)
+                .input(cableGtSingle, PedotTma, 2)
+                .fluidInputs(SolderingAlloy.getFluid(L * 4))
+                .fluidInputs(Lubricant.getFluid(1000))
+                .fluidInputs(StyreneButadieneRubber.getFluid(L * 24))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(CONVEYOR_MODULE_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void piston() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(ELECTRIC_MOTOR_UEV)
+                .input(plate, MetallicHydrogen, 4)
+                .input(ring, MetallicHydrogen, 4)
+                .input(round, MetallicHydrogen, 16)
+                .input(stick, MetallicHydrogen, 4)
+                .input(gear, MetallicHydrogen)
+                .input(gearSmall, MetallicHydrogen, 2)
+                .input(cableGtSingle, PedotTma, 2)
+                .fluidInputs(SolderingAlloy.getFluid(L * 4))
+                .fluidInputs(Lubricant.getFluid(1000))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(ELECTRIC_PISTON_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void robotArm() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(stickLong, MetallicHydrogen, 4)
+                .input(gear, MetallicHydrogen)
+                .input(gearSmall, MetallicHydrogen, 3)
+                .input(ELECTRIC_MOTOR_UEV, 2)
+                .input(ELECTRIC_PISTON_UEV)
+                .input(circuit, MarkerMaterials.Tier.UEV)
+                .input(circuit, MarkerMaterials.Tier.UHV, 2)
+                .input(circuit, MarkerMaterials.Tier.UV, 4)
+                .input(cableGtSingle, PedotTma, 4)
+                .fluidInputs(SolderingAlloy.getFluid(L * 12))
+                .fluidInputs(Lubricant.getFluid(1000))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(ROBOT_ARM_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void pump() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(ELECTRIC_MOTOR_UEV)
+                .input(pipeLargeFluid, Neutronium)
+                .input(plate, MetallicHydrogen, 2)
+                .input(screw, MetallicHydrogen, 8)
+                .input(ring, StyreneButadieneRubber, 16)
+                .input(rotor, MetallicHydrogen)
+                .input(cableGtSingle, PedotTma, 2)
+                .fluidInputs(SolderingAlloy.getFluid(L * 4))
+                .fluidInputs(Lubricant.getFluid(1000))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(ELECTRIC_PUMP_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void emitter() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(frameGt, MetallicHydrogen)
+                .input(ELECTRIC_MOTOR_UEV)
+                .input(stickLong, MetallicHydrogen, 4)
+                .input(GRAVI_STAR)
+                .input(circuit, MarkerMaterials.Tier.UEV, 2)
+                .input(foil, MercuryCadmiumTelluride, 64)
+                .input(foil, MercuryCadmiumTelluride, 32)
+                .input(cableGtSingle, PedotTma, 4)
+                .fluidInputs(SolderingAlloy.getFluid(L * 8))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(EMITTER_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void sensor() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(frameGt, MetallicHydrogen)
+                .input(ELECTRIC_MOTOR_UEV)
+                .input(plate, MetallicHydrogen, 4)
+                .input(GRAVI_STAR)
+                .input(circuit, MarkerMaterials.Tier.UEV, 2)
+                .input(foil, Fullerene, 64)
+                .input(foil, Fullerene, 32)
+                .input(cableGtSingle, PedotTma, 4)
+                .fluidInputs(SolderingAlloy.getFluid(L * 8))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(SENSOR_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+
+    private static void fieldGenerator() {
+        ASSEMBLY_LINE_RECIPES.recipeBuilder()
+                .input(frameGt, MetallicHydrogen)
+                .input(plate, MetallicHydrogen, 6)
+                .input(GRAVI_STAR)
+                .input(EMITTER_UEV, 2)
+                .input(circuit, MarkerMaterials.Tier.UHV, 2)
+                .input(wireFine, CarbonNanotube, 64)
+                .input(wireFine, CarbonNanotube, 64)
+                .input(cableGtSingle, PedotTma, 4)
+                .fluidInputs(SolderingAlloy.getFluid(L * 12))
+                .fluidInputs(Neutronium.getFluid(L * 4))
+                .output(FIELD_GENERATOR_UEV)
+                .duration(600).EUt(1600000).buildAndRegister();
+    }
+    
     private static void craftingComponents() {
         ModHandler.addShapedRecipe("component_grinder_boron_nitride", GCMetaItems.COMPONENT_GRINDER_BORON_NITRIDE.getStackForm(),
                 "PDP", "DGD", "PDP",
